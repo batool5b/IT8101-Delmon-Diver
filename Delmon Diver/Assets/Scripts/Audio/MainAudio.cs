9b19713ce3813d845c4c120acda16ce407e33e71
MohamedJaafar0 <202304048@student.polytechnic.bh>
2026-04-22 14:20:54 +0300

    settings

--- Delmon Diver/Assets/Scripts/Audio/MainAudio.cs
+++ Delmon Diver/Assets/Scripts/Audio/MainAudio.cs
@@ -1,11 +1,37 @@
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
+ 
 public class MainAudio : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        MusicManager.Instance.PlayMusic("A");
-    }
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+ 
 
+ 
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+ 
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+ 
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+ 
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+ 
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+    }
 }
